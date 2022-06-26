07251de865a878dae97a1413152846bec75e09e2
jd <j.judd.singer@gmail>
2022-06-26 16:27:51 -0400

    used scanner and for loop to complete lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner myscanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+
+        int n = myscanner.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+
+
+        System.out.println(sum);
 
     }
 }
